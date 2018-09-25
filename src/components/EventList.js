e4a565093ff7fcf59d58d910f918ebc98e498427
ccareyncsu <ccarey@ncsu.edu>
2018-09-25 15:54:04 -0600

    eventlist on dashboard populated from currentUser

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import './styles/EventList.css'
 
 export function EventList(props) {
-  
+  console.log(props.events);
+  const event = props.events.map((event, i)=>{
+    console.log(event);
+    return(
+      <div key={i} className='user-event'>
+        <p>{event.title}</p>
+        <p>{event.description}</p>
+      </div>
+    )
+  })
+
   return (
     <div className='event-list'>
-      <div className='user-event'>
-        <p>10</p>
-        <p>Active Events</p>
-      </div>
+      {event}
     </div>
   )
 }
 
 const mapStateToProps= state => ({
-  event: state.auth.currentUser
+  events: state.auth.currentUser.eventList
 })
 
 export default connect(mapStateToProps)(EventList);
