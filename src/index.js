19ad43e31d0d11e0bcecb3f199fd294ea8d15273
ccareyncsu <ccarey@ncsu.edu>
2018-09-25 12:20:06 -0600

    added create event and eventlist

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux';
 import store from './Store';
+import { CreateEvent } from './components/CreateEvent';
+
 
 
 ReactDOM.render(
 
     <Provider store={store}>
-             <App />
+             <CreateEvent />
     </Provider>, 
 
 document.getElementById('root'));
