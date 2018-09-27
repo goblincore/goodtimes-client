2179fd918e736605d87265095f8be93dfd3af872
Jonny Cheng <jcheng02@mica.edu>
2018-09-27 12:43:39 -0400

    copy link to clipboard

--- src/components/Events/SuccessfullyCreatedEvent.js
+++ src/components/Events/SuccessfullyCreatedEvent.js
@@ -1,21 +1,52 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 
+export default class SuccessfullyCreatedEvent extends React.Component {
 
-export default function SuccessfullyCreatedEvent(props) {
-
-  return (
-      <div className="event-successfully-created">
-
-        <h2>Nice! Your event has been created successfully.</h2>
-
-        <div className="event-link-to-share">
-            <h3>Share this link with your friends:</h3>
-            <p id="event-link">http://weekends.herokuapp.com/events/{props.eventState.id}</p>
+  constructor(props) {
+    super(props);
+    this.state={
+      value:'',
+      copied:false
+    }
   
-            <button id="back-to-dashboard" onClick={() => props.nextPage()}>Back to Dashboard</button>
-        </div>
+  }
+  
+  
+  handleCopy =()=>{
+    console.log("copy")
+    var inp =document.createElement('input');
+    document.body.appendChild(inp)
+    inp.value =document.querySelector("#event-link").textContent
+    inp.select();
+    document.execCommand('copy',false);
+    inp.remove();
+    this.setState({copied:true});
+    
+  }
 
-      </div>
-  )
+  
+
+  render(){
+      return (
+        <div className="event-successfully-created">
+
+          <h2>Nice! Your event has been created successfully.</h2>
+
+          <div className="event-link-to-share">
+              <h3>Share this link with your friends:</h3>
+              {/* {props.eventState.id} */}
+              <p id="event-link">http://goodtimes.herokuapp.com/events/123123example</p>
+
+              <button onClick={this.handleCopy}>Copy Link</button>  
+              {this.state.copied ? <span style={{color: 'red'}}><p>Copied</p></span> : null}
+               <p></p>
+              <Link to="/dashboard"> <button id="back-to-dashboard" >Back to Dashboard</button></Link>
+          </div>
+
+        </div>
+    )
+  } 
+ 
 }
