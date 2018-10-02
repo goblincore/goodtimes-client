2923d2839910618855e581d0dd5b5c0165279c13
Jennifer Colna <jennifer.colna@gmail.com>
2018-10-02 14:15:26 -0500

    emails send with user input

--- src/components/Events/SuccessfullyCreatedEvent.js
+++ src/components/Events/SuccessfullyCreatedEvent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import { CLIENT_BASE_URL } from '../../config';
 import { resetNewEventState } from '../../actions/New-Event';
+import { sendEmail } from '../../actions/Email';
 
 
 export default class SuccessfullyCreatedEvent extends React.Component {
@@ -33,6 +34,18 @@ export default class SuccessfullyCreatedEvent extends React.Component {
     this.setState({copied:true});
   }
 
+  sendEmail(e){
+    e.preventDefault();
+    const recipients = e.target.to.value.split(',');
+    console.log(recipients);
+    this.props.dispatch(sendEmail({
+                to: recipients,
+                from: e.target.from.value,
+                subject: e.target.subject.value,
+                text: e.target.message.value,
+                html: `<p>${e.target.message.value}</p>`
+              }));
+  }
   
 
   render(){
@@ -40,14 +53,20 @@ export default class SuccessfullyCreatedEvent extends React.Component {
       <div className="event-successfully-created">
 
         <h2>Nice! Your event has been created successfully.</h2>
-
+        <button onClick={this.handleCopy}>Copy Link</button>  
+          {this.state.copied ? <span style={{color: 'red'}}><p>Copied</p></span> : null}
+            <p></p>
         <div className="event-link-to-share">
           <h3>Share this link with your friends:</h3>
           <p id="event-link">{CLIENT_BASE_URL}/guestevents/{this.props.eventState.id}</p>
-
-          <button onClick={this.handleCopy}>Copy Link</button>  
-          {this.state.copied ? <span style={{color: 'red'}}><p>Copied</p></span> : null}
-            <p></p>
+          <h3>Email an invite to your friends!</h3>
+          <form onSubmit={(e) => this.sendEmail(e)}>
+            <input placeholder='user@example.com' id='from'></input>
+            <input placeholder="Enter your friends' e-mails, separated by a comma." id="to"></input>
+            <input placeholder='Subject Line' id="subject"></input>
+            <textarea placeholder="Hi!  Let's get together. Please vote on when and where we should hang out." id="message"></textarea>
+            <button type="submit">Send</button>
+          </form>
           <Link to="/dashboard"> 
             <button id="back-to-dashboard">
                 Back to Dashboard
