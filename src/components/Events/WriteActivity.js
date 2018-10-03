35e2a0e761242fd4019e9e328fcdf4620cd5045c
Jennifer Colna <jennifer.colna@gmail.com>
2018-10-03 14:28:46 -0500

    modularizing activity select

---
+++ src/components/Events/WriteActivity.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { fetchCategories, fetchActivities } from '../../actions/Activities';
+import { updateNewEventState } from '../../actions/New-Event';
+import moment from 'moment';
+
+export default class WriteActivity extends React.Component {
+  render(){
+    return (
+      <div>write</div>
+    );
+  }
+}
