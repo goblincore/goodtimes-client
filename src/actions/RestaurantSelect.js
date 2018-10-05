18d0e91391556c2e5c9b26697e016540abcc51b9
Jennifer Colna <jennifer.colna@gmail.com>
2018-10-05 14:44:57 -0500

    removed zomato actions and reducers

--- src/actions/RestaurantSelect.js
+++ src/actions/RestaurantSelect.js
@@ -1,67 +1,5 @@
-import {API_BASE_URL, CLIENT_BASE_URL} from '../config';
+import {API_BASE_URL} from '../config';
 import {normalizeResponseErrors} from './Utils';
-
-export const FETCH_ZOMATO_LOCATION_REQUEST = 'FETCH_ZOMATO_LOCATION_REQUEST';
-export const fetchZomatoLocationRequest = () => ({
-  type: FETCH_ZOMATO_LOCATION_REQUEST
-});
-
-export const FETCH_ZOMATO_LOCATION_SUCCESS = 'FETCH_ZOMATO_LOCATION_SUCCESS';
-export const fetchZomatoLocationSuccess = (cityCode) => ({
-  type: FETCH_ZOMATO_LOCATION_SUCCESS,
-  cityCode
-});
-
-export const FETCH_ZOMATO_LOCATION_ERROR = 'FETCH_ZOMATO_LOCATION_ERROR';
-export const fetchZomatoLocationError = (error) => ({
-  type: FETCH_ZOMATO_LOCATION_ERROR,
-  error
-});
-export const fetchZomatoLocation = (lat, lon) => (dispatch, getState) => {
-  dispatch(fetchZomatoLocationRequest());
-  return fetch(`${API_BASE_URL}/api/restaurants/${lat}/${lon}`, {
-    method: 'GET'
-  })
-    .then(res => normalizeResponseErrors(res))
-    .then(res => res.json())
-    .then(cityCode => {
-      dispatch(fetchZomatoLocationSuccess(cityCode));
-      dispatch(fetchCuisines(cityCode.id));
-    })
-    .catch(err => dispatch(fetchZomatoLocationError(err)));
-};
-
-export const FETCH_CUISINES_REQUEST = 'FETCH_CUISINES_REQUEST';
-export const fetchCuisinesRequest = () => ({
-  type: FETCH_CUISINES_REQUEST
-});
-
-export const FETCH_CUISINES_SUCCESS = 'FETCH_CUISINES_SUCCESS';
-export const fetchCuisinesSuccess = (cuisines) => ({
-  type: FETCH_CUISINES_SUCCESS,
-  cuisines
-});
-
-export const FETCH_CUISINES_ERROR = 'FETCH_CUISINES_ERROR';
-export const fetchCuisinesError = (error) => ({
-  type: FETCH_CUISINES_ERROR,
-  error
-});
-
-export const fetchCuisines = (cityCode) => (dispatch) => {
-  dispatch(fetchCuisinesRequest());
-  return fetch(`https://developers.zomato.com/api/v2.1/cuisines?city_id=${cityCode}`, {
-    method: 'GET',
-    headers: {
-      'Accept':'application/json',
-      'user-key':'02fb4b75d2055eb17f988da8447de24a',
-    },
-  })
-    .then(res => normalizeResponseErrors(res))
-    .then(res => res.json())
-    .then(cuisines => dispatch(fetchCuisinesSuccess(cuisines)))
-    .catch(err => dispatch(fetchCuisinesError(err)));
-};
 
 export const FETCH_YELP_CATEGORIES_REQUEST = 'FETCH_YELP_CATEGORIES_REQUEST';
 export const fetchYelpCategoriesRequest = () => ({
@@ -117,39 +55,6 @@ export const fetchYelpRestaurants = (category, lat, lon) => (dispatch) => {
     .then(restaurants => dispatch(fetchYelpRestaurantsSuccess(restaurants)))
     .catch(err => dispatch(fetchYelpRestaurantsError(err)));
 };
-
-export const FETCH_RESTAURANTS_REQUEST = 'FETCH_RESTAURANTS_REQUEST';
-export const fetchRestaurantsRequest = () => ({
-  type: FETCH_RESTAURANTS_REQUEST
-});
-
-export const FETCH_RESTAURANTS_SUCCESS = 'FETCH_RESTAURANTS_SUCCESS';
-export const fetchRestaurantsSuccess = (restaurants) => ({
-  type: FETCH_RESTAURANTS_SUCCESS,
-  restaurants
-});
-
-export const FETCH_RESTAURANTS_ERROR = 'ETCH_RESTAURANTS_ERROR';
-export const fetchRestaurantsError = (error) => ({
-  type: FETCH_RESTAURANTS_ERROR,
-  error
-});
-
-export const fetchRestaurants = (cityCode, cuisine) => (dispatch) => {
-  dispatch(fetchRestaurantsRequest());
-  return fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=${cityCode}&entity_type=city&cuisines=${cuisine}`, {
-    method: 'GET',
-    headers: {
-      'Accept':'application/json',
-      'user-key':'02fb4b75d2055eb17f988da8447de24a',
-    }, 
-  })
-    .then(res => normalizeResponseErrors(res))
-    .then(res => res.json())
-    .then(restaurants => dispatch(fetchRestaurantsSuccess(restaurants)))
-    .catch(err => dispatch(fetchRestaurantsError(err)));
-};
-
 
 export const RESET_RESTAURANTS_REDUCER = 'RESET_RESTAURANTS_REDUCER';
 export const resetRestaruantsReducer = () => ({
--- src/reducers/RestaurantSelect.js
+++ src/reducers/RestaurantSelect.js
@@ -12,72 +12,14 @@ import {
 } from '../actions/RestaurantSelect';
 
 const initialState = {
-  cityCode:null,
-  cuisines:[],
   yelpCategories:[],
   loading: false,
   error: null,
-  restaurants: [],
   yelpRestaurants: []
 };
 
 export default function reducer(state = initialState, action){
-  if(action.type === FETCH_ZOMATO_LOCATION_REQUEST){
-    return Object.assign({}, state, {
-      loading: true
-    });
-  }
-  else if(action.type === FETCH_ZOMATO_LOCATION_ERROR){
-    return Object.assign({}, state, {
-      loading: false,
-      error: action.error
-    });
-  }
-  else if(action.type === FETCH_ZOMATO_LOCATION_SUCCESS){
-    return Object.assign({}, state, {
-      loading: false,
-      cityCode: action.cityCode.id
-    });
-  }
-  else if(action.type === FETCH_CUISINES_REQUEST){
-    return Object.assign({}, state, {
-      loading: true
-    });
-  }
-  else if(action.type === FETCH_CUISINES_ERROR){
-    return Object.assign({}, state, {
-      loading: false,
-      error: action.error
-    });
-  }
-  else if(action.type === FETCH_CUISINES_SUCCESS){
-    console.log(action);
-    return Object.assign({}, state, {
-      loading: false,
-      cuisines: action.cuisines.cuisines
-    });
-  }
-  else if(action.type === FETCH_RESTAURANTS_REQUEST){
-    console.log(action);
-    return Object.assign({}, state, {
-      loading: true
-    });
-  }
-  else if(action.type === FETCH_RESTAURANTS_ERROR){
-    console.log(action);
-    return Object.assign({}, state, {
-      loading: false,
-      error: action.error
-    });
-  }
-  else if(action.type === FETCH_RESTAURANTS_SUCCESS){
-    console.log(action.restaurants.restaurants);
-    return Object.assign({}, state, {
-      loading: false,
-      restaurants:  action.restaurants.restaurants
-    });
-  } 
-  else if (action.type === RESET_RESTAURANTS_REDUCER) {
+  if(action.type === RESET_RESTAURANTS_REDUCER) {
     return Object.assign({}, state, initialState);
   }
   else if(action.type === FETCH_YELP_CATEGORIES_REQUEST){
