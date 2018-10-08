3d8ce230a8f242d612551ec46910c4e7dbb4bcbf
Jonny Cheng <jcheng02@mica.edu>
2018-10-08 10:54:15 -0400

    removed first name last name fields

--- src/components/HomePage/Register/RegistrationForm.js
+++ src/components/HomePage/Register/RegistrationForm.js
@@ -14,8 +14,8 @@ export class RegistrationForm extends React.Component {
     super(props);
     this.randomId=uuidv4();
     this.usernameId=uuidv4();
-    this.firstnameId=uuidv4();
-    this.lastnameId=uuidv4();
+    // this.firstnameId=uuidv4();
+    // this.lastnameId=uuidv4();
     this.passwordId=uuidv4();
     this.emailAddressId=uuidv4();
     this.matchesPassword = matches(this.passwordId);
@@ -23,11 +23,11 @@ export class RegistrationForm extends React.Component {
   onSubmit(values) {
     const username = values[this.usernameId];
     const password = values[this.passwordId];
-    const lastName = values[this.lastnameId];
-    const firstName = values[this.firstnameId];
+    // const lastName = values[this.lastnameId];
+    // const firstName = values[this.firstnameId];
     const email = values[this.emailAddressId];
 
-    const user = {username, password, firstName, lastName, email};
+    const user = {username, password, email};
     return this.props
       .dispatch(registerUser(user))
       .then(() => this.props.dispatch(login(username, password)));
@@ -44,10 +44,16 @@ export class RegistrationForm extends React.Component {
         onSubmit={this.props.handleSubmit(values =>
           this.onSubmit(values)
         )}>
-        <label htmlFor={this.firstnameId}>First name</label>
-        <Field component={Input} type="text" name={this.firstnameId} />
-        <label htmlFor={this.lastnameId}>Last name</label>
-        <Field component={Input} type="text" name={this.lastnameId} />
+
+         <label htmlFor={this.usernameId}>Username</label>
+        <Field
+          component={Input}
+          autofocus
+          type="text"
+          name={this.usernameId}
+          validate={[required, nonEmpty, isTrimmed]}
+        />
+      
         <label htmlFor={this.usernameId}>Email</label>
         <Field
           component={Input}
@@ -56,14 +62,7 @@ export class RegistrationForm extends React.Component {
           name={this.emailAddressId}
           validate={[required, nonEmpty, isTrimmed]}
         />
-        <label htmlFor={this.usernameId}>Username</label>
-        <Field
-          component={Input}
-          autofocus
-          type="text"
-          name={this.usernameId}
-          validate={[required, nonEmpty, isTrimmed]}
-        />
+       
         <label htmlFor={this.passwordId}>Password</label>
         <Field
           component={Input}
