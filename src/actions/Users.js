2cd408f152a5c7990c0b8627d641a4010c87f043
MorganHuegel <mhue2910@gmail.com>
2018-10-08 14:37:17 -0400

    added server error message for registration form

--- src/actions/Users.js
+++ src/actions/Users.js
@@ -2,7 +2,7 @@ import {SubmissionError} from 'redux-form';
 
 import {API_BASE_URL} from '../config';
 import {normalizeResponseErrors} from './Utils';
-import { authRequest, login } from './Auth';
+import { authRequest, login, authError } from './Auth';
 
 export const registerUser = user => dispatch => {
     dispatch(authRequest());  //set loading to true while waiting
@@ -17,14 +17,6 @@ export const registerUser = user => dispatch => {
         .then(res => res.json())
         .then(() => dispatch(login(user.username, user.password)))
         .catch(err => {
-            const {reason, message, location} = err;
-            if (reason === 'ValidationError') {
-                // Convert ValidationErrors into SubmissionErrors for Redux Form
-                return Promise.reject(
-                    new SubmissionError({
-                        [location]: message
-                    })
-                );
-            }
+          dispatch(authError(err.message));
         });
 };
--- src/components/HomePage/Register/RegistrationForm.js
+++ src/components/HomePage/Register/RegistrationForm.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import {Field, reduxForm, focus} from 'redux-form';
 import {registerUser} from '../../../actions/Users';
-import {login} from '../../../actions/Auth';
 import Input from '../../ReusableComponents/Input';
 import {required, nonEmpty, matches, length, isTrimmed} from '../../../Validators';
 const passwordLength = length({min: 10, max: 72});
-// const matchesPassword = matches('password');
 const uuidv4 = require('uuid/v4');
 
 export class RegistrationForm extends React.Component {
@@ -14,8 +12,6 @@ export class RegistrationForm extends React.Component {
     super(props);
     this.randomId=uuidv4();
     this.usernameId=uuidv4();
-    this.firstnameId=uuidv4();
-    this.lastnameId=uuidv4();
     this.passwordId=uuidv4();
     this.emailAddressId=uuidv4();
     this.matchesPassword = matches(this.passwordId);
@@ -23,14 +19,11 @@ export class RegistrationForm extends React.Component {
   onSubmit(values) {
     const username = values[this.usernameId];
     const password = values[this.passwordId];
-    const lastName = values[this.lastnameId];
-    const firstName = values[this.firstnameId];
     const email = values[this.emailAddressId];
 
-    const user = {username, password, firstName, lastName, email};
+    const user = {username, password, email};
     return this.props
       .dispatch(registerUser(user))
-      .then(() => this.props.dispatch(login(username, password)));
   }
 
   render() {
@@ -44,10 +37,9 @@ export class RegistrationForm extends React.Component {
         onSubmit={this.props.handleSubmit(values =>
           this.onSubmit(values)
         )}>
-        <label htmlFor={this.firstnameId}>First name</label>
-        <Field component={Input} type="text" name={this.firstnameId} />
-        <label htmlFor={this.lastnameId}>Last name</label>
-        <Field component={Input} type="text" name={this.lastnameId} />
+
+        <p>{this.props.errorMessage}</p>
+        
         <label htmlFor={this.usernameId}>Email</label>
         <Field
           component={Input}
@@ -56,6 +48,7 @@ export class RegistrationForm extends React.Component {
           name={this.emailAddressId}
           validate={[required, nonEmpty, isTrimmed]}
         />
+
         <label htmlFor={this.usernameId}>Username</label>
         <Field
           component={Input}
@@ -64,6 +57,7 @@ export class RegistrationForm extends React.Component {
           name={this.usernameId}
           validate={[required, nonEmpty, isTrimmed]}
         />
+
         <label htmlFor={this.passwordId}>Password</label>
         <Field
           component={Input}
@@ -71,6 +65,7 @@ export class RegistrationForm extends React.Component {
           name={this.passwordId}
           validate={[required, passwordLength, isTrimmed]}
         />
+
         <label htmlFor="passwordConfirm">Confirm password</label>
         <Field
           component={Input}
@@ -78,11 +73,12 @@ export class RegistrationForm extends React.Component {
           name="passwordConfirm"
           validate={[required, nonEmpty, this.matchesPassword]}
         />
+
         <div className='align-right'>
           <button
             type="submit"
             disabled={this.props.pristine || this.props.submitting}>
-                        Register
+              Register
           </button>
         </div>
       </form>
@@ -91,7 +87,5 @@ export class RegistrationForm extends React.Component {
 }
 
 export default reduxForm({
-  form: 'registration',
-  onSubmitFail: (errors, dispatch) =>
-    dispatch(focus('registration', Object.keys(errors)[0]))
+  form: 'registration'
 })(RegistrationForm);
