e11413de28866167fc6e5f1b0b290cd8c30cca57
William Holcombe <sphinxelite@Williams-iMac.local>
2018-10-09 15:47:21 -0400

    about to add test helper

---
+++ src/components/EventCreation/Page2-DateTime/DateList.test.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import  DateList  from  './DateList';
+import {shallow, mount} from 'enzyme';
+
+
+const dateList = [ 
+
+{date: "Tue, Oct 30, 2018 11:30 AM",
+id: "5bb63b5bca7fba0a6ef3f847",
+votes: 0
+},
+{ 
+date:
+"Tue, Oct 30, 2018 3:30 PM",
+id:
+"5bb63b5bca7fba0a6ef3f846",
+votes:
+0},
+{
+date:
+"Tue, Oct 9, 2018 9:30 PM",
+id:
+"5bb63b5bca7fba0a6ef3f845",
+votes:
+0
+}
+];
+
+
+function setup() {
+  const props = {
+      dateList: dateList,
+   eventState: {
+    location:{
+      latitude: jest.fn(),
+      longitude: jest.fn()
+    },
+    restaurantOptions: jest.fn(),
+    scheduleOptions: jest.fn(),
+   },
+   restaurants: {
+     yelpRestaurants: jest.fn()
+   },
+    newEvent: 
+     {
+      restaurants: {
+        yelpRestaurants: jest.fn()
+      }
+    },
+
+    dispatch: jest.fn()
+  }
+  const localStorage = jest.fn();
+  const enzymeWrapper = mount(<DateList {...props} localStorage={localStorage}/>)
+
+
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
+describe('components', () => {
+  describe('DateList', () => {
+    it('should render self without crashing and list dates', () => {
+      const { enzymeWrapper } = setup();
+
+      enzymeWrapper.find('.date-list-item').forEach((node) => {
+        expect(node.hasClass('date-list-item')).to.equal(true);
+      });
+      //expect(enzymeWrapper.find('#date-list-item')).to.have.lengthOf(3);
+      //expect(enzymeWrapper.find('p').text()).toEqual('No time and dates added yet!');
+    })
+  })
+})
---
+++ src/components/EventCreation/Page2-DateTime/DateSelectPage.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import  DateSelectPage  from  './DateSelectPage';
+import {shallow, mount} from 'enzyme';
+const renderer = require('react-test-renderer');
+
+function setup() {
+  const props = {
+   eventState: {
+    location:{
+      latitude: jest.fn(),
+      longitude: jest.fn()
+    },
+    restaurantOptions: jest.fn(),
+    scheduleOptions: jest.fn(),
+   },
+   restaurants: {
+     yelpRestaurants: jest.fn()
+   },
+    newEvent: 
+     {
+      restaurants: {
+        yelpRestaurants: jest.fn()
+      }
+    },
+
+    dispatch: jest.fn()
+  }
+
+  
+  const enzymeWrapper = mount(<DateSelectPage {...props} />)
+
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
+describe('components', () => {
+  describe('DateSelectPage', () => {
+    it('should render self and not Datelist if there are no dates to populate', () => {
+      const { enzymeWrapper } = setup();
+     
+      expect(enzymeWrapper.find('DateList').exists()).toBe(false);
+    })
+        // it('matches the snapshot', () => {
+        //     const tree = renderer.create(<DateSelectPage />).toJSON()
+        //     expect(tree).toMathSnapshot()
+        // })
+  })
+})
