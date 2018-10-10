bdcddc8f143d5f1410fe5853d12e4f131ece4c0c
Jonny Cheng <jcheng02@mica.edu>
2018-10-09 23:15:21 -0400

    colors

--- src/App.js
+++ src/App.js
@@ -178,7 +178,7 @@ const mapStateToProps = state => ({
 
 const HomePage = ({ style,...props }) => (
   
-  <animated.div className="mainRoute" style={{ ...style, background: `#fdfdfd` }}>
+  <animated.div className="mainRoute" style={{ ...style, background: `#1f4bff` }}>
     <div className="mainRouteItem">
      <LandingPage />  
     </div>
@@ -186,7 +186,7 @@ const HomePage = ({ style,...props }) => (
 );
 
 const RegisterPage = ({ style }) => (
-    <animated.div className="mainRoute bg2" style={{ ...style, background: `#fdfdfd` }}>
+    <animated.div className="mainRoute bg2" style={{ ...style, background: `#dcff60` }}>
       <div className="mainRouteItem">
      <RegistrationPage />
       </div>
